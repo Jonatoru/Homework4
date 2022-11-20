ff03a5e338049ff4efe431e2b7407ae8db2db93b
Jonatoru <jetrence95@gmail.com>
2022-11-20 11:59:00 +0500

    Выполнил Задачу 2

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,28 @@ public class Main {
         if (old >= ageLimit) {
             System.out.println( "Поздравляем вас с совершеннолетием!");
         }
-        byte old2 = 17;
-        if (old2 < ageLimit) {
+        byte old1 = 17;
+        if (old1 < ageLimit) {
             System.out.println( "Возраст совереннолетия ещё не наструпил, нужно немного подождать.");
         }
 
+        System.out.println("Задача 2");
+
+        byte old2 = 7;
+        byte ageLimitSchool = 7;
+        if (old2 >= ageLimitSchool) {
+            System.out.println("Ребенок ходит в школу.");
+        }
+        byte old3 = 18;
+        byte ageLimitStudent = 18;
+        if (old3 >= ageLimitStudent) {
+            System.out.println("Человек уже закончил школу и может отправляться в университет.");
+        }
+        byte old4 = 24;
+        byte ageLimitWorker = 24;
+        if (old4 >= ageLimitWorker) {
+            System.out.println("Человек уже закончил университет и ему пора искать первую работу.");
+        }
 
 
     }
